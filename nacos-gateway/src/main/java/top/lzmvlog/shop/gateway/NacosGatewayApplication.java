7202e4ae67e21fc6a7acb55317583089aab611f2
Shao Jie <zhang1591313226@163.com>
2021-02-21 14:41:51 +0800

    Docker打包测试

--- nacos-gateway/src/main/java/top/lzmvlog/shop/gateway/NacosGatewayApplication.java
+++ nacos-gateway/src/main/java/top/lzmvlog/shop/gateway/NacosGatewayApplication.java
@@ -3,7 +3,6 @@ package top.lzmvlog.shop.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
