d1acec438763059d2b5a2228a24979399dd29b03
chenghao <cheng_hao24@163.com>
2021-02-24 23:12:53 +0800

    修改mybatisplus配置类

--- common/src/main/java/top/lzmvlog/common/config/MybatisPlusConfiguration.java
+++ common/src/main/java/top/lzmvlog/common/config/MybatisPlusConfiguration.java
@@ -2,24 +2,27 @@ package top.lzmvlog.common.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * mybatis - plus 分页配置
+ * mybatis-plus配置类
+ *
  * @author zhang1591313226@163.com
  * @since 2021-02-23 16:35
  */
-@EnableTransactionManagement
 @Configuration
-public class PageConfiguration {
+public class MybatisPlusConfiguration {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        // 拒绝全表更新与删除
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        // 分页配置
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
 }
