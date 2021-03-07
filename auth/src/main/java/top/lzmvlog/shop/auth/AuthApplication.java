7ce9f09093481d2af4aa49a5272223874473f031
Shao Jie <zhang1591313226@163.com>
2021-03-07 21:41:34 +0800

    feat: 新增auth服务 fix: shop-files文件服务日志路径修改 subject: 新增auth服务

---
+++ auth/src/main/java/top/lzmvlog/shop/auth/AuthApplication.java
@@ -0,0 +1,20 @@
+package top.lzmvlog.shop.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author ShaoJie zhang1591313226@163.com
+ * @Date 2021年03月07日 21:30
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
